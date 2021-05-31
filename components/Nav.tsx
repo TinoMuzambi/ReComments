4431b8c0e571310df6fc93f6b7f960b5ef775a5c
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-31 21:12:46 +0200

    Implements sign out functionality.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,11 @@
+import { useContext } from "react";
 import Link from "next/link";
 
+import { AppContext } from "../context/AppContext";
+
 const Nav = () => {
+	const { setSignedIn, signedIn } = useContext(AppContext);
+
 	return (
 		<header className="search-header">
 			<nav className="nav">
@@ -8,9 +13,16 @@ const Nav = () => {
 					<Link href="/">
 						<a className="link">Home</a>
 					</Link>
-					<Link href="/">
-						<a className="link">Sign Out</a>
-					</Link>
+					<li
+						className="link"
+						onClick={() => {
+							if (signedIn) {
+								if (setSignedIn) setSignedIn(false);
+							}
+						}}
+					>
+						Sign Out
+					</li>
 					<Link href="/">
 						<a className="link">GitHub</a>
 					</Link>
