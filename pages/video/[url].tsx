d9713d8b3195f5f0bd64262d7dee2676a3045b18
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-31 20:26:45 +0200

    Adds custom meta for video.

--- pages/video/[url].tsx
+++ pages/video/[url].tsx
@@ -2,6 +2,7 @@ import { /*GetStaticProps, GetStaticPaths,*/ GetServerSideProps } from "next";
 import parse from "html-react-parser";
 
 import { VideoProps } from "../../interfaces";
+import Meta from "../../components/Meta";
 
 const Video = ({
 	title,
@@ -17,24 +18,31 @@ const Video = ({
 	html,
 }: VideoProps) => {
 	return (
-		<main className="video-holder">
-			<h1 className="title">{title}</h1>
-			{embeddable ? (
-				<div className="player">{parse(html)}</div>
-			) : (
-				<div className="player">
-					<img src={thumbnail} alt={title} className="thumbnail" />
+		<>
+			<Meta
+				title={`${title} | ReComments`}
+				description={`Comment on '${title}' from ${channel} on ReComments!`}
+				url={`https://re-comments.vercel.app/video/${id}`}
+			/>
+			<main className="video-holder">
+				<h1 className="title">{title}</h1>
+				{embeddable ? (
+					<div className="player">{parse(html)}</div>
+				) : (
+					<div className="player">
+						<img src={thumbnail} alt={title} className="thumbnail" />
+					</div>
+				)}
+				<div className="stats">
+					<p className="stat">{views}</p>
+					<p className="stat">{likes}</p>
+					<p className="stat">{dislikes}</p>
+					<p className="stat">{new Date(date).toLocaleDateString()}</p>
 				</div>
-			)}
-			<div className="stats">
-				<p className="stat">{views}</p>
-				<p className="stat">{likes}</p>
-				<p className="stat">{dislikes}</p>
-				<p className="stat">{new Date(date).toLocaleDateString()}</p>
-			</div>
-			<h3 className="uploader">{channel}</h3>
-			<p className="desc">{description}</p>
-		</main>
+				<h3 className="uploader">{channel}</h3>
+				<p className="desc">{description}</p>
+			</main>
+		</>
 	);
 };
 
