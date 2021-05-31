46ff9c94857abfc5ffdb10d47984b88e17a06706
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-31 18:16:09 +0200

    Changes push to go to search.

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 	const signIn = () => {
 		authenticate(() => {
 			if (setSignedIn) setSignedIn(true);
-			router.push("/videos");
+			router.push("/search");
 		}).then(loadClient());
 	};
 
