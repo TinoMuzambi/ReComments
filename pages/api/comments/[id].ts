2d4a099c93938d352a35e17cdf625c10875d1811
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-02 13:54:05 +0200

    Adds get by id route.

---
+++ pages/api/comments/[id].ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import Comment from "../../../models/Comment";
+import dbConnect from "../../../utils/dbConnect";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	const {
+		query: { id },
+		method,
+	} = req;
+
+	switch (method) {
+		case "GET":
+			try {
+				const comment = await Comment.findById(id);
+
+				if (!comment) {
+					return res.status(400).json({ success: false });
+				}
+
+				res.status(200).json({ success: true, data: comment });
+			} catch (error) {
+				return res.status(400).json({ success: false });
+			}
+	}
+};
