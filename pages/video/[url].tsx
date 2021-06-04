11d5c04ee9f17df4db41a20d04d8b50994f6ff96
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-04 12:29:23 +0200

    Adds main tag for styling.

--- pages/video/[url].tsx
+++ pages/video/[url].tsx
@@ -26,14 +26,16 @@ const Video: React.FC = () => {
 
 	if (!result)
 		return (
-			<div className="error-holder">
-				<img
-					src="https://a.storyblok.com/f/114267/1222x923/8898eb61f4/error.png"
-					alt="error"
-					className="error-image"
-				/>
-				<h1 className="error">Loading...</h1>
-			</div>
+			<main className="main">
+				<div className="error-holder">
+					<img
+						src="https://a.storyblok.com/f/114267/1222x923/8898eb61f4/error.png"
+						alt="error"
+						className="error-image"
+					/>
+					<h1 className="error">Loading...</h1>
+				</div>
+			</main>
 		);
 	return (
 		<>
