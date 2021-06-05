cb363fb0c807a284e94a7aefd1cc23edd8a12eb9
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-05 19:36:46 +0200

    Adds holders.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -19,21 +19,25 @@ const Nav: React.FC = () => {
 		<header className="nav-header">
 			<nav className="nav">
 				<ul className="links">
-					<h1 className="logo">R</h1>
-					<Link href="/">
-						<a className="link">Home</a>
-					</Link>
-					{signedIn && (
-						<li className="link" onClick={handleSignOut}>
-							Sign Out
-						</li>
-					)}
-					{user && user.photos && user.names && (
-						<div className="profile">
-							<img className="photo" src={user.photos[0].url} alt="Profile" />
-							<p className="name">{user.names[0].givenName}</p>
-						</div>
-					)}
+					<div className="logo-holder">
+						<h1 className="logo">R</h1>
+					</div>
+					<div className="links-holder">
+						<Link href="/">
+							<a className="link">Home</a>
+						</Link>
+						{signedIn && (
+							<li className="link" onClick={handleSignOut}>
+								Sign Out
+							</li>
+						)}
+						{user && user.photos && user.names && (
+							<div className="profile">
+								<img className="photo" src={user.photos[0].url} alt="Profile" />
+								<p className="name">{user.names[0].givenName}</p>
+							</div>
+						)}
+					</div>
 				</ul>
 			</nav>
 		</header>
