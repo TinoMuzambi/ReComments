a748e76a4db71111e727ca4ffc6f5edf96c957b5
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-09 23:26:00 +0200

    Updates schema.

--- models/Comment.ts
+++ models/Comment.ts
@@ -4,18 +4,47 @@ import { CommentModel } from "../interfaces";
 
 const CommentSchema: Schema = new mongoose.Schema(
 	{
-		authorId: {
+		videoId: {
 			type: String,
-			required: [true, "Comment needs an author."],
+			required: [true, "Comment needs a video ID."],
+			unique: true,
 			trim: true,
 		},
-		text: {
+		email: {
+			type: String,
+			required: [true, "Comment needs an email"],
+			unique: true,
+			trim: true,
+		},
+		comment: {
 			type: String,
 			required: [true, "Comment needs comment text."],
 		},
-		videoId: {
+		name: {
 			type: String,
-			ref: "Video",
+			required: [true, "Comment needs a name"],
+			trim: true,
+		},
+		image: {
+			type: String,
+			trim: true,
+			default:
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSisT6Nb6XIXyX7kQ9XmEMID6eSxl4mQ8E0vXbwc77pJqhZYUUdU13h7VRlt4rZqOgg5Yc&usqp=CAU",
+		},
+		upvotes: {
+			type: Number,
+			default: 0,
+		},
+		downvotes: {
+			type: Number,
+			default: 0,
+		},
+		mention: {
+			type: String || null,
+			default: null,
+		},
+		replies: {
+			type: [this],
 		},
 	},
 	{
