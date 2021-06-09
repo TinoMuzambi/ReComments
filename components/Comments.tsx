2b694dd57d41b15c68620c5e6b1cead119284786
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-09 22:23:11 +0200

    Types comment.

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,10 +1,11 @@
 import comments from "../data/comments.json";
 import Comment from "./Comment";
+// import { CommentInterface } from "../interfaces";
 
 const Comments = () => {
 	return (
 		<section className="comments">
-			{comments.map((comment) => (
+			{comments.map((comment: any) => (
 				<Comment comment={comment} key={comment.id} />
 			))}
 		</section>
