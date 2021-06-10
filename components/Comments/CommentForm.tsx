dcf794270ecb53f84b013c9fbb2973341f66a949
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-10 14:50:16 +0200

    Adds markup for form.

--- components/Comments/CommentForm.tsx
+++ components/Comments/CommentForm.tsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 const CommentForm = () => {
-	return <form>Form</form>;
+	const [opened, setOpened] = useState(false);
+
+	return (
+		<article className="form-holder">
+			<img src="" alt="" className="profile" />
+			<form className="form">
+				<input type="text" className="text" onFocus={() => setOpened(true)} />
+				{opened && (
+					<div className="buttons">
+						<button className="cancel">Cancel</button>
+						<button type="submit">Comment</button>
+					</div>
+				)}
+			</form>
+		</article>
+	);
 };
 
 export default CommentForm;
