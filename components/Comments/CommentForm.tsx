3986aab40a1d6ec974f68c1b5fa81c62c49711b6
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-10 14:45:37 +0200

    Adds form to comments section.

---
+++ components/Comments/CommentForm.tsx
@@ -0,0 +1,5 @@
+const CommentForm = () => {
+	return <form>Form</form>;
+};
+
+export default CommentForm;
--- components/Comments/Comments.tsx
+++ components/Comments/Comments.tsx
@@ -1,4 +1,5 @@
 import Comment from "./Comment";
+import CommentForm from "./CommentForm";
 // import { CommentInterface } from "../interfaces";
 
 const Comments: React.FC<any> = ({ comments }) => {
@@ -8,6 +9,7 @@ const Comments: React.FC<any> = ({ comments }) => {
 				<h5 className="total">{comments.length} Comments</h5>
 				<button className="sort">Sort by</button>
 			</div>
+			<CommentForm />
 			{comments.map((comment: any) => (
 				<Comment comment={comment} key={comment.id} />
 			))}
