9a257fda3b87aa6d2bdd73bf6f9a3efd485e73d5
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-11 02:22:31 +0200

    Changes id route to use _id.

--- pages/api/comments/[id].ts
+++ pages/api/comments/[id].ts
@@ -13,7 +13,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 	switch (method) {
 		case "GET":
 			try {
-				const comment: Comment[] = await Comment.find({ videoId: id });
+				const comment: Comment[] = await Comment.find({ _id: id });
 
 				if (!comment) {
 					return res.status(404).json({ success: false });
