c4964ad9f48e51f67ae8fb854d6e1cda908467b3
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-11 20:55:59 +0200

    Adds use create index true.

--- utils/dbConnect.ts
+++ utils/dbConnect.ts
@@ -12,6 +12,7 @@ const dbConnect = async () => {
 	const db = await mongoose.connect(process.env.MONGO_URI || "", {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
+		useCreateIndex: true,
 	});
 
 	connection.isConnected = db.connections[0].readyState;
