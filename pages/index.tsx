483e61b907e3085d3491f813f21087756cdc69dc
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-11 05:54:33 +0200

    Adds fetch to post user.

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,6 +88,14 @@ export default function Home() {
 					createdAt: new Date(),
 					updatedAt: new Date(),
 				};
+
+				await fetch("/api/users", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(body),
+				});
 			}
 		}
 	};
