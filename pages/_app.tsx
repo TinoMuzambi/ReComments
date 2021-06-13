773e338a5eb8b29638001abb9c2be80eb28e2bad
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-13 12:13:40 +0200

    Converts app to arrow function.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,12 @@ import { AppProps } from "next/app";
 import Wrapper from "../components/Wrapper";
 import "../sass/App.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: Function = ({ Component, pageProps }: AppProps) => {
 	return (
 		<Wrapper>
 			<Component {...pageProps} />
 		</Wrapper>
 	);
-}
+};
 
 export default MyApp;
