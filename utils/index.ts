c8283400057e16e9faacdd2b539b2ebb83187d44
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-18 01:21:08 +0200

    Adds shuffle function.

--- utils/index.ts
+++ utils/index.ts
@@ -48,3 +48,24 @@ export const VOTING_TYPES = {
 export const numberWithCommas: Function = (x: number): string => {
 	return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 };
+
+export const shuffle: Function = (array: string[]): string[] => {
+	// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+	let currentIndex = array.length,
+		randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex],
+		];
+	}
+
+	return array;
+};
