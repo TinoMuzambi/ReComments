61b0e91bf8880aad137907ec07f138f966dcf263
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-18 00:14:25 +0200

    Adds popular conditionals.

--- utils/gapi.ts
+++ utils/gapi.ts
@@ -74,14 +74,20 @@ export const execute: Function = async (
 			const data = await res.json();
 			response = data.data.videos;
 		}
-
-		if (setResults) setResults(response.result.items);
+		console.log(response);
+		if (popular) {
+			if (setResults) setResults(response);
+		} else {
+			if (setResults) setResults(response.result.items);
+		}
 		if (setFetching) setFetching(false);
-		if (response.result.pageInfo) {
-			if (response.result.pageInfo.totalResults === 0) {
-				if (setNoResults) setNoResults(true);
-			} else {
-				if (setNoResults) setNoResults(false);
+		if (!popular) {
+			if (response.result.pageInfo) {
+				if (response.result.pageInfo.totalResults === 0) {
+					if (setNoResults) setNoResults(true);
+				} else {
+					if (setNoResults) setNoResults(false);
+				}
 			}
 		}
 	} catch (err) {
