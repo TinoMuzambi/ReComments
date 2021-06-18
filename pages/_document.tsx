9f3bd3146720f4e344583d5b948c985c5e073dfb
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-18 23:09:42 +0200

    Adds toggle button.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,24 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { MouseEventHandler } from "react";
+import { useState } from "react";
 
 class MyDocument extends Document {
 	render() {
+		let dark: boolean = false;
+
+		const toggleDarkMode: MouseEventHandler<HTMLButtonElement> = () => {
+			dark = !dark;
+		};
+
 		return (
 			<Html lang="en" prefix="og: http://ogp.me/ns#">
 				<Head>
 					<script src="https://apis.google.com/js/api.js"></script>
 				</Head>
-				<body>
+				<body className={`${dark && "dark"}`}>
+					<button className="toggle" onClick={toggleDarkMode}>
+						Dark
+					</button>
 					<Main />
 					<NextScript />
 				</body>
