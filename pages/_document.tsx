b6bb08abb815a1245b168f4dd1e8cece377479a5
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-19 01:50:06 +0200

    Removes logic from _document.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { MouseEventHandler } from "react";
 
 class MyDocument extends Document {
 	render() {
-		let dark: boolean = false;
-
-		const toggleDarkMode: MouseEventHandler<HTMLButtonElement> = () => {
-			dark = !dark;
-		};
-
 		return (
 			<Html lang="en" prefix="og: http://ogp.me/ns#">
 				<Head>
 					<script src="https://apis.google.com/js/api.js"></script>
 				</Head>
-				<body className={`${dark && "dark"}`}>
-					<button className="toggle" onClick={toggleDarkMode}>
-						Dark
-					</button>
+				<body>
 					<Main />
 					<NextScript />
 				</body>
