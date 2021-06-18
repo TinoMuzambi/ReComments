a1711d32fc7655ce7c1778c36c26cb07f143b30d
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-18 13:48:40 +0200

    Adds spinner.

---
+++ components/Spinner.tsx
@@ -0,0 +1,11 @@
+const Spinner = () => {
+	return (
+		<div className="loadingio-spinner-eclipse-f4oz4cwoap8">
+			<div className="ldio-mz5ahte0vxn">
+				<div></div>
+			</div>
+		</div>
+	);
+};
+
+export default Spinner;
