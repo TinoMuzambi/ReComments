e84a761ac5a87e10c420f8c9c11cfb8c7fe42be1
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-19 01:54:50 +0200

    Moves functionality into wrapper.

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -1,13 +1,31 @@
+import { MouseEventHandler, useEffect, useState } from "react";
+
 import { WrapperProps } from "../interfaces";
 import { AppProvider } from "../context/AppContext";
 import Meta from "./Meta";
 import Nav from "./Nav";
 
 const Wrapper: React.FC<WrapperProps> = ({ children }): JSX.Element => {
+	const [dark, setDark] = useState(false);
+
+	useEffect(() => {
+		if (dark) {
+			document.body.classList.add("dark");
+		} else {
+			document.body.classList.remove("dark");
+		}
+	}, [dark]);
+
+	const toggleDarkMode: MouseEventHandler<HTMLButtonElement> = () => {
+		setDark(!dark);
+	};
 	return (
 		<AppProvider>
 			<Meta />
 			<Nav />
+			<button className="toggle" onClick={toggleDarkMode}>
+				Dark
+			</button>
 			{children}
 		</AppProvider>
 	);
