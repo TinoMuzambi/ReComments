a08a6301ba1c88238409ca786a80f64c312af1ae
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-19 04:22:33 +0200

    Changes html and text.

--- pages/api/email/index.ts
+++ pages/api/email/index.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import nodemailer from "nodemailer";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const { to, commentText, url } = req.body;
+	const { to, fromName, commentText, url } = req.body;
 	let transporter = nodemailer.createTransport({
 		service: "gmail",
 		auth: {
@@ -13,9 +13,19 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
 	const options = {
 		from: "tinomuzambi@gmail.com",
-		to: "t56muzambi@gmail.com",
-		subject: "Test 1 2 3",
-		text: "Tino was here.",
+		to: to,
+		subject: "New reply to your comment",
+		text: `${fromName} replied to your comment on ReComments. Paste this url ${url} in the search box on ReComments to view the reply.`,
+		html: `
+			<header>
+				<img src="https://a.storyblok.com/f/114267/1080x1080/b66aa450e5/recomments.png" alt="logo"/>
+			</header>
+			<main>
+				<h1>New reply to your comment</h1>
+				<br><br>
+				<p>${fromName} replied to your comment on ReComments.</p>
+				<p>Paste this url <a href=${url} target="_blank">${url}</a> in the search box on ReComments to view the reply.</p>
+			</main>`,
 	};
 
 	transporter.sendMail(options, (err, info) => {
