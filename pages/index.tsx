541f6256b4544a6f1d9c8016b8f455ca6ef95637
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-19 16:16:07 +0200

    Adds text content.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ const index = () => {
 	return (
 		<main className="main index">
 			<div className="topbar">
-				<h1 className="title">Welcome to ReComments</h1>
+				<h1 className="title">
+					Welcome to <span className="accent">ReComments</span>
+				</h1>
 				<Link href="/signin">
 					<a className="action">Sign In</a>
 				</Link>
@@ -12,24 +14,35 @@ const index = () => {
 			<section className="about">
 				<h2 className="subtitle">About</h2>
 				<p className="text">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-					dolore ad omnis non soluta deleniti perferendis eius modi asperiores
-					vitae nostrum rerum, quibusdam, magni eveniet fuga blanditiis
-					aspernatur nam laboriosam inventore sunt suscipit necessitatibus
-					incidunt! Officiis repudiandae dolorum architecto vitae veniam quis
-					dignissimos doloribus quam. Ullam ipsa excepturi nobis deserunt?
+					<span className="accent">ReComments</span> is a site designed to bring
+					back the ability to comment on YouTube videos where the uploader has
+					decided to turn them off. You simply paste the video url on the search
+					page then the video will be fetched. Once you navigate to the video,
+					you'll be able to play the video on{" "}
+					<span className="accent">ReComments</span> as well as see the stats
+					for the video (such as number of likes/dislikes/views). But more
+					importantly, you'll now be able to comment on the video (keep it
+					friendly though). <span className="accent">ReComments </span>
+					will also notify you via email (promise no spam) when people reply to
+					your comments.
 				</p>
 			</section>
 			<section className="method">
 				<h2 className="subtitle">How it works</h2>
 				<h5 className="tag">The technical stuff</h5>
 				<p className="text">
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id
-					dignissimos eaque consequatur velit voluptatibus qui beatae tempora
-					tempore minus iste similique, a laborum incidunt deserunt. Vel quam
-					delectus fugit eaque amet. Numquam mollitia nesciunt, esse corporis
-					tempore deserunt quos voluptatum ex voluptas facilis officia quia unde
-					error omnis minus iusto!
+					<span className="accent">ReComments</span> uses the Google People API
+					to facilitate signing into the site as well as storing only the
+					necessary user data. Once you're signed in,{" "}
+					<span className="accent">ReComments</span> uses the Google YouTube API
+					to fetch video data.
+				</p>
+				<p className="text">
+					<span className="accent">ReComments</span> is implemented using
+					Next.js with TypeScript for the front-end. Styling is done through
+					Sass. The back-end is comprised of Next.js serverless functions. The
+					back-end handles storing user/comment data in a MongoDB database as
+					well as handling sending emails via Nodemailer.
 				</p>
 			</section>
 		</main>
