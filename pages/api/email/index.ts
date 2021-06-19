a4b6689c06774e1c445138cb7f3f438415194bf9
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-19 04:44:58 +0200

    Adds more html styles.

--- pages/api/email/index.ts
+++ pages/api/email/index.ts
@@ -16,19 +16,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const options = {
 		from: "tinomuzambi@gmail.com",
 		to: to,
-		subject: "New reply to your comment",
+		subject: "ReComments | New reply to your comment",
 		text: `${fromName} replied to your comment on ReComments. They said: "${commentText}". Paste this url ${url} in the search box on ReComments to continue the conversation.`,
 		html: `
 			<header>
 				<img src="https://a.storyblok.com/f/114267/1080x1080/b66aa450e5/recomments.png" alt="logo"/>
 			</header>
 			<main>
-				<h1>New reply to your comment</h1>
-				<br><br>
-				<p>${fromName} replied to your comment on ReComments.</p>
+				<h1>New reply to your comment on ReComments</h1>
+				<p><b>${fromName}</b> replied to your comment on ReComments.</p>
 				<p>They said:</p>
 				<blockquote>${commentText}</blockquote>
-				<p>Paste this url <a href=${url} target="_blank">${url}</a> in the search box on ReComments to continue the conversation.</p>
+				<p>Paste this url <a href=${url} target="_blank">${url}</a> in the search box on <a href="https://recomments.tinomuzambi.com" target="_blank">ReComments</a> to continue the conversation.</p>
 			</main>
 			<style>
 				* {
@@ -38,6 +37,25 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 				header img {
 					height: 100px;
 				}
+
+				blockquote {
+					background:#f9f9f9;
+					border-left:10px solid #ccc;
+					margin:1.5em 10px;
+					padding:.5em 10px;
+					white-space: pre-wrap;
+				}
+
+				blockquote:before {
+					color:#ccc;
+					content:open-quote;
+					font-size:4em;
+					line-height:.1em;
+					margin-right:.25em;
+					vertical-align:-.4em;
+				}
+
+				
 			</style>
 			`,
 	};
@@ -52,8 +70,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 				inf = info;
 			}
 		});
+		return res.status(200).json({ success: true, data: inf });
 	} catch (error) {
 		return res.status(400).json({ success: false, error: error });
 	}
-	return res.status(200).json({ success: true, data: inf });
 };
