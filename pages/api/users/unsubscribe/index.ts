1c433a92e81b24e2a2d13f90cf0f6b70b2837dfb
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-21 16:12:33 +0200

    Adds unsubscribe api route.

---
+++ pages/api/users/unsubscribe/index.ts
@@ -0,0 +1,66 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import mongoose from "mongoose";
+
+import User from "../../../../models/User";
+import dbConnect from "../../../../utils/dbConnect";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	dbConnect();
+	const {
+		query: { id },
+		method,
+	} = req;
+
+	switch (method) {
+		case "GET":
+			try {
+				const user: typeof User = await User.findOne({ userId: id });
+
+				if (!user) {
+					return res
+						.status(404)
+						.json({ success: false, data: { message: "User not found" } });
+				}
+
+				res.status(200).json({ success: true, data: user });
+			} catch (error) {
+				return res.status(400).json({ success: false, data: error });
+			}
+			break;
+		case "PUT":
+			try {
+				const user: mongoose.UpdateQuery<any> = await User.updateOne(
+					{ _id: id },
+					{ ...req.body }
+				);
+
+				if (!user) {
+					return res
+						.status(400)
+						.json({ success: false, data: { message: "User not found" } });
+				}
+				res.status(200).json({ success: true, data: user });
+			} catch (error) {
+				return res.status(400).json({ success: false, data: error });
+			}
+			break;
+		case "DELETE":
+			try {
+				const deletedUser = await User.deleteOne({ _id: id });
+
+				if (!deletedUser) {
+					return res
+						.status(400)
+						.json({ success: false, data: { message: "User not found" } });
+				}
+				res
+					.status(200)
+					.json({ success: true, data: { message: "User deleted" } });
+			} catch (error) {
+				return res.status(400).json({ success: false, data: error });
+			}
+			break;
+		default:
+			return res.status(400).json({ success: false });
+	}
+};
