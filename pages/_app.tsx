f48d53b3c1b2692d39e01a063b51d1b1df29336a
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-25 16:59:17 +0200

    Disable dev tools in prod.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
 import { AppProps } from "next/app";
+import { useEffect } from "react";
 
 import Wrapper from "../components/Wrapper";
 import "../sass/App.scss";
 
 const MyApp: Function = ({ Component, pageProps }: AppProps) => {
+	useEffect(() => {
+		const disableReactDevTools = (): void => {
+			const noop = (): void => undefined;
+			const DEV_TOOLS = (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__;
+
+			if (typeof DEV_TOOLS === "object") {
+				for (const [key, value] of Object.entries(DEV_TOOLS)) {
+					DEV_TOOLS[key] = typeof value === "function" ? noop : null;
+				}
+			}
+		};
+		if (process.env.NODE_ENV === "production") disableReactDevTools();
+	}, []);
 	return (
 		<Wrapper>
 			<Component {...pageProps} />
