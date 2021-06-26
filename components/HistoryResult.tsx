f63631df6bc9a710b32aa7fd7c44ebc4ef9c7592
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-27 01:25:26 +0200

    Adds explainer text.

--- components/HistoryResult.tsx
+++ components/HistoryResult.tsx
@@ -10,7 +10,9 @@ const HistoryResult: React.FC<{ item: HistoryItem }> = ({ item }) => {
 					<img src={item.thumbnail} alt={item.title} />
 					<h3 className="name">{item.title}</h3>
 					<h5 className="uploader">{item.uploader}</h5>
-					<h6 className="date">{moment(item.date).fromNow()}</h6>
+					<h6 className="date">
+						You watched this {moment(item.date).fromNow()}
+					</h6>
 				</div>
 			</a>
 		</Link>
