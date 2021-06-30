be7ac4c8a6712900b26463e433249a868e28d02f
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-30 12:59:21 +0200

    Updates class names.

--- components/AppState.tsx
+++ components/AppState.tsx
@@ -3,13 +3,13 @@ import { AppStateProps } from "../interfaces";
 const Loading: React.FC<AppStateProps> = ({ message }): JSX.Element => {
 	return (
 		<main className="main">
-			<div className="error-holder">
+			<div className="holder">
 				<img
 					src="https://a.storyblok.com/f/114267/1222x923/8898eb61f4/error.png"
-					alt="error"
-					className="error-image"
+					alt={message}
+					className="image"
 				/>
-				<h1 className="error">{message}</h1>
+				<h1 className="message">{message}</h1>
 			</div>
 		</main>
 	);
