159ddd13874ea76446f513aa496cec366abada10
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-30 13:04:20 +0200

    Removes class name.

--- components/AppState.tsx
+++ components/AppState.tsx
@@ -8,7 +8,7 @@ const Loading: React.FC<AppStateProps> = ({ message }): JSX.Element => {
 					src="https://a.storyblok.com/f/114267/1222x923/8898eb61f4/error.png"
 					alt={message}
 				/>
-				<h1 className="message">{message}</h1>
+				<h1>{message}</h1>
 			</div>
 		</main>
 	);
