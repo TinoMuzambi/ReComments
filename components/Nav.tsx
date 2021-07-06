0a8cb5277d2c4ccdd1b1b4682064998721064464
Tino Muzambi <tinomuzambi@gmail.com>
2021-07-07 00:14:45 +0200

    Moves toggler into nav.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,16 +1,48 @@
 import { MouseEventHandler, useContext, useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { IoMdMoon, IoMdSunny } from "react-icons/io";
 
 import { AppContext } from "../context/AppContext";
 import { handleSignoutClick } from "../utils/gapi";
+import { UserModel } from "../interfaces";
+import { postUpdatedResourceToDb } from "../utils";
 
 const Nav: React.FC = (): JSX.Element => {
 	const [navOpen, setNavOpen] = useState(false);
+	const [dark, setDark] = useState(false);
 
 	const { setSignedIn, setUser, dbUser, signedIn, setDbUser } =
 		useContext(AppContext);
 	const router = useRouter();
+
+	useEffect(() => {
+		// If present set it.
+		if (dbUser?.darkMode) {
+			setDark(dbUser?.darkMode);
+			if (dbUser?.darkMode) document.body.classList.add("dark");
+			else document.body.classList.remove("dark");
+		}
+	}, [dbUser]);
+
+	const toggleDarkMode: MouseEventHandler<HTMLButtonElement> = async () => {
+		console.log(dbUser);
+		let updatedUser: UserModel = { ...(dbUser as UserModel) };
+		if (dark) {
+			// console.log(updatedUser);
+			document.body.classList.remove("dark");
+			updatedUser.darkMode = false;
+			await postUpdatedResourceToDb(updatedUser);
+			// console.log(updatedUser);
+		} else {
+			// console.log(updatedUser);
+			document.body.classList.add("dark");
+			updatedUser.darkMode = true;
+			await postUpdatedResourceToDb(updatedUser);
+			// console.log(updatedUser);
+		}
+		setDark(!dark);
+	};
 
 	useEffect(() => {
 		// Hide navigation on route change.
@@ -36,58 +68,67 @@ const Nav: React.FC = (): JSX.Element => {
 	};
 
 	return (
-		<header className="nav-header">
-			<nav className={`nav ${navOpen && "open"}`}>
-				<ul className={`links ${navOpen && "open"}`}>
-					<div className="row">
-						<div className="logo-holder">
-							<Link href="/search">
-								<a className="link">
-									<h1 className="logo">R</h1>
-								</a>
-							</Link>
+		<>
+			<button
+				data-dark={dark ? "Turn on light mode" : "Turn on dark mode"}
+				className="toggle"
+				onClick={toggleDarkMode}
+			>
+				{dark ? <IoMdSunny className="icon" /> : <IoMdMoon className="icon" />}
+			</button>
+			<header className="nav-header">
+				<nav className={`nav ${navOpen && "open"}`}>
+					<ul className={`links ${navOpen && "open"}`}>
+						<div className="row">
+							<div className="logo-holder">
+								<Link href="/search">
+									<a className="link">
+										<h1 className="logo">R</h1>
+									</a>
+								</Link>
+							</div>
+							<div
+								className={`burger ${navOpen && "open"}`}
+								onClick={() => setNavOpen(!navOpen)}
+							>
+								<div className="top"></div>
+								<div className="middle"></div>
+								<div className="bottom"></div>
+							</div>
 						</div>
-						<div
-							className={`burger ${navOpen && "open"}`}
-							onClick={() => setNavOpen(!navOpen)}
-						>
-							<div className="top"></div>
-							<div className="middle"></div>
-							<div className="bottom"></div>
+						<div className={`links-holder ${navOpen && "open"}`}>
+							{signedIn && (
+								<Link href="/search">
+									<a className="link">Home</a>
+								</Link>
+							)}
+							<Link href="/">
+								<a className="link">About</a>
+							</Link>
+							{signedIn && (
+								<li className="link" onClick={handleSignOut}>
+									Sign Out
+								</li>
+							)}
+							{dbUser && (
+								<Link href="/profile">
+									<a className="link">
+										<div className="profile">
+											<img
+												className="photo"
+												src={dbUser.photoUrl}
+												alt={dbUser.shortName}
+											/>
+											<p className="name">{dbUser.shortName}</p>
+										</div>
+									</a>
+								</Link>
+							)}
 						</div>
-					</div>
-					<div className={`links-holder ${navOpen && "open"}`}>
-						{signedIn && (
-							<Link href="/search">
-								<a className="link">Home</a>
-							</Link>
-						)}
-						<Link href="/">
-							<a className="link">About</a>
-						</Link>
-						{signedIn && (
-							<li className="link" onClick={handleSignOut}>
-								Sign Out
-							</li>
-						)}
-						{dbUser && (
-							<Link href="/profile">
-								<a className="link">
-									<div className="profile">
-										<img
-											className="photo"
-											src={dbUser.photoUrl}
-											alt={dbUser.shortName}
-										/>
-										<p className="name">{dbUser.shortName}</p>
-									</div>
-								</a>
-							</Link>
-						)}
-					</div>
-				</ul>
-			</nav>
-		</header>
+					</ul>
+				</nav>
+			</header>
+		</>
 	);
 };
 
--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -1,48 +1,14 @@
-import { MouseEventHandler, useEffect, useState } from "react";
-import { IoMdMoon, IoMdSunny } from "react-icons/io";
-
 import { WrapperProps } from "../interfaces";
 import { AppProvider } from "../context/AppContext";
 import Meta from "./Meta";
 import Nav from "./Nav";
 
 const Wrapper: React.FC<WrapperProps> = ({ children }): JSX.Element => {
-	const [dark, setDark] = useState(false);
-
-	useEffect(() => {
-		// Get dark mode stat from local storage.
-		const darkLocalStorage = JSON.parse(
-			localStorage.getItem("recomments-dark") as string
-		);
-		// If present set it.
-		if (darkLocalStorage === true || darkLocalStorage === false) {
-			setDark(darkLocalStorage);
-			if (darkLocalStorage) document.body.classList.add("dark");
-			else document.body.classList.remove("dark");
-		}
-	}, []);
-
-	const toggleDarkMode: MouseEventHandler<HTMLButtonElement> = () => {
-		if (dark) {
-			document.body.classList.remove("dark");
-			localStorage.setItem("recomments-dark", JSON.stringify(false));
-		} else {
-			document.body.classList.add("dark");
-			localStorage.setItem("recomments-dark", JSON.stringify(true));
-		}
-		setDark(!dark);
-	};
 	return (
 		<AppProvider>
 			<Meta />
 			<Nav />
-			<button
-				data-dark={dark ? "Turn on light mode" : "Turn on dark mode"}
-				className="toggle"
-				onClick={toggleDarkMode}
-			>
-				{dark ? <IoMdSunny className="icon" /> : <IoMdMoon className="icon" />}
-			</button>
+
 			{children}
 		</AppProvider>
 	);
