0c408a0c6f1c72d800f5abeb971c3bb619131ada
Tino Muzambi <tinomuzambi@gmail.com>
2021-07-06 23:51:26 +0200

    Adds toggle.

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -302,6 +302,26 @@ const Profile: React.FC = (): JSX.Element => {
 						/>
 					</div>
 					<div className="input-group">
+						<label className="check-toggle" htmlFor="uniqueID">
+							<input type="checkbox" className="toggle__input" id="uniqueID" />
+							<span className="toggle-track">
+								<span className="toggle-indicator">
+									<span className="checkMark">
+										<svg
+											viewBox="0 0 24 24"
+											id="ghq-svg-check"
+											role="presentation"
+											aria-hidden="true"
+										>
+											<path d="M9.86 18a1 1 0 01-.73-.32l-4.86-5.17a1.001 1.001 0 011.46-1.37l4.12 4.39 8.41-9.2a1 1 0 111.48 1.34l-9.14 10a1 1 0 01-.73.33h-.01z"></path>
+										</svg>
+									</span>
+								</span>
+							</span>
+							Enabled toggle label
+						</label>
+					</div>
+					<div className="input-group">
 						<button type="submit">Save</button>
 						<button type="reset">Delete your account</button>
 					</div>
