d20e82bc378f7dfd174cebe2758984d39b4851e6
Tino Muzambi <tinomuzambi@gmail.com>
2021-07-22 22:36:06 +0200

    Adds push back to search if not auth.

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,4 +1,18 @@
+import { useEffect, useContext } from "react";
+import { useRouter } from "next/router";
+
+import { AppContext } from "../context/AppContext";
+import { ROLES } from "../utils";
+
 const Admin: React.FC = (): JSX.Element => {
+	const { dbUser } = useContext(AppContext);
+
+	const router = useRouter();
+
+	useEffect(() => {
+		if (dbUser?.role === ROLES.standard) router.push("/search");
+	}, [dbUser]);
+
 	return <main className="main">Admin</main>;
 };
 
